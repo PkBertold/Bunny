a57c683870bb52377b583efde22aa74a637074d5
leventenemecz <nemecanita0@gmail.com>
2020-10-06 18:16:43 +0200

    safwe

---
+++ paratlan.py
@@ -0,0 +1,6 @@
+def szovegParatlan(szoveg):
+szovegParos=""
+	szovegParatlan = ("acegikmoqstvxz")
+	for x in range(0,len(szoveg)-1,1):
+		szovegParos+=szoveg[x]
+	return SzovegParatlan
---
+++ paros.py
@@ -0,0 +1,7 @@
+# Nemec Anita	
+def szovegParos(szoveg):
+szovegParos=""
+	szovegParos = ("bdfhjlnqsuwy")
+	for x in range(0,len(szoveg)-1,2):
+		szovegParos+=szoveg[x]
+	return SzovegParos
