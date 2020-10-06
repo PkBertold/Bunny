fc54f2b7b757c2ecea05ec9c48ee737c300a9ac9
Ladányi Bertold <bertold95pk@gmail.com>
2020-10-06 18:36:42 +0200

    Paratlan fixed

--- paratlan.py
+++ paratlan.py
@@ -2,5 +2,5 @@ def szovegParatlan(szoveg):
 szovegParos=""
 	szovegParatlan = ("acegikmoqstvxz")
 	for x in range(0,len(szoveg)-1,1):
-		szovegParos+=szoveg[x]
+		szovegParatlan+=szoveg[x]
 	return SzovegParatlan
