67c5746652f7b0239ba190b470c8722d3ac4c56c
Szoftver <sisina.akemi@gmail.com>
2020-10-06 17:47:57 +0200

    hála az égnek sikerült

--- szoveg.py
+++ szoveg.py
@@ -1,2 +1,8 @@
 szoveg=input("Írj be egy szöveget: ")
 	print(szovegFordit(szoveg))
+	
+def szovegFordit(szoveg):
+	fordSzoveg="
+	for x in range(,len(szoveg)-1,-1 ,-1):
+		fordSzoveg+=szoveg[x]
+	return (forditszoveg)
