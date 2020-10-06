339bc8308515b4c8e755aab6014348fb59c3808a
Ladányi Bertold <bertold95pk@gmail.com>
2020-10-06 18:21:53 +0200

    szoveg.py frissítve

--- szoveg.py
+++ szoveg.py
@@ -2,9 +2,22 @@ def szovegFordit(szoveg):
 	fordSzoveg=""
 	for x in range(len(szoveg)-1,-1,-1):
 		fordSzoveg+=szoveg[x]
-	return szovegFordit
+	return fordSzoveg
 	
-szoveg=input("Írj be egy szöveget: ")
+def szovegCsere(szoveg):
+	maganHangzo="aáéoöüóiíÍAÁÉOÖÜÓIÍ"
+	for x in range(0,len(maganHangzo)):
+		szoveg=szoveg.replace(maganHangzo[x],"e");
+	return szoveg
+	
+def szovegParos(szoveg):
+	parosSzoveg = ""
+	for x in range (0,len(szoveg),2):
+		parosSzoveg += szoveg[x]
+	return parosSzoveg
+	
+szoveg=input("Írj be egy szöveget:")
 print(szovegFordit(szoveg))
-
-
+print(szovegCsere(szoveg))
+print(szovegParos(szoveg))
+print(szovegParatlan(szoveg))
